13848c36d62166994a6a31993cc9579c022a1d5d
Nirmal Limbu <nirmalmaswa@live.com>
2015-08-30 17:35:04 +0545

    Button Changing textview

--- app/src/main/java/com/example/max/myapplication/MainActivity.java
+++ app/src/main/java/com/example/max/myapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView txtHello = (TextView) findViewById(R.id.textView);
+
+        ImageButton btnFirst = (ImageButton) findViewById(R.id.imageButton);
+
+        btnFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtHello.setText("Goodbye");
+            }
+        });
     }
 
     @Override
